910942341b8ac7e575b497fee156975fb917ab90
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-07-26 02:00:42 +0900

    [add] getWeatherData.js
    データベースを介さずにそのままEmbedに書き出すやつ

--- getWeatherData.js
+++ getWeatherData.js
@@ -1,35 +1,67 @@
 const https = require("https");
 const path = require("path");
 const fs = require("fs");
+const Discord = require("discord.js");
+require('date-utils');
 
 require('dotenv').config({path: path.join(__dirname, '.env')});
 
-async function downloadJson(areaCode) {
-    return new Promise((resolve, reject) => {
-        const url = `https://www.jma.go.jp/bosai/forecast/data/forecast/${areaCode}.json`;
-        https.get(url, (res) => {
-            let body = '';
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => body += chunk);
-            res.on('end', () => resolve(JSON.parse(body)));
-        }).on('error', (e) => reject(e.message));
+const downloadJson = async areaCode => new Promise((resolve, reject) => {
+    const url = `https://www.jma.go.jp/bosai/forecast/data/forecast/${areaCode}.json`;
+    https.get(url, (res) => {
+        let body = '';
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => body += chunk);
+        res.on('end', () => resolve(JSON.parse(body)));
+    }).on('error', (e) => reject(e.message));
+});
+
+const openCodesFile = async () => new Promise((resolve, reject) => {
+    fs.readFile('./weatherCodes.json', 'utf8', (err, file) => {
+        resolve(JSON.parse(file));
+    });
+});
+
+getWeatherData = async primaryAreaCode => {
+    Promise.all([
+        downloadJson(primaryAreaCode), // 天気予報のjsonを取ってくる
+        openCodesFile(), // テロップ番号対応表
+    ]).then((values) => {
+        const tenki = values[0];
+        const codes = values[1];
+
+        const optionDate = Date.tomorrow();
+        const reportDatetime = new Date(tenki[0].srf.reportDatetime);
+
+        // 該当の日付がリストの何番目にあるか探す
+        const optionIndex = tenki[0].srf.timeSeries[0].timeDefines.findIndex(element => Date.equals(new Date(element), optionDate))
+
+        // Embedの初期化
+        let forecast = new Discord.MessageEmbed()
+            .setTitle(optionDate.toFormat('YYYY年MM月DD日') + 'の天気')
+            .setColor('0x219ddd')
+            .setDescription(reportDatetime.toFormat('YYYY年MM月DD日 HH24時MI分') + ' 気象庁発表')
+            .setURL('https://www.jma.go.jp/bosai/forecast/');
+
+        // ここループでぶん回して全都市引っこ抜く
+
+        tenki.forEach(city => {
+
+            const name = city.name;
+            const weatherCode = city.srf.timeSeries[0].areas.weatherCodes[optionIndex];
+
+            let weatherName, weatherEmoji;
+            for (let key in codes) {
+                if (key === weatherCode) {
+                    weatherName = codes[key][0];
+                    weatherEmoji = codes[key][2];
+                }
+            }
+            forecast.addField(name, weatherEmoji + '\n' + weatherName, true);
+        });
+
+        console.log(forecast);
     });
 }
 
-const wcList = JSON.parse(fs.readFileSync('./input.json', 'utf8'));
-
-updateDatabase = async primaryAreaCode => {
-    Promise.all([downloadJson(areaCode), promise2, promise3]).then((values) => {
-        console.log(values);
-    });
-    downloadJson(primaryAreaCode).then((jsonData) => {
-        // 取ってきた天気予報のjsonを開く
-
-        // テロップ番号対応表を開く
-
-        // 何月何日何時気象庁発表
-
-    })
-}
-
-updateDatabase('010000').then(() => console.log('success!!!!!!')).catch((res, err) => console.log(res, err))
+getWeatherData('010000').then(() => console.log('success!!!!!!')).catch((res, err) => console.log(res, err))
