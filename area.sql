490aaa39495d5cfc7998c04eeec868720e5e4533
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-07-26 01:56:56 +0900

    [del] .sql files
    いらんので

--- area.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "public"."kushiro" (
-    "id" serial not null,
-    "report_datetime" timestamp,
-    "date_define" timestamp UNIQUE,
-    "weather_code" text,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
-);
