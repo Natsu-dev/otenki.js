6fb509a5aec5acb5a688284b7fcf80daa8d2cc6d
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-07-28 02:21:12 +0900

    [upd] info command
    infoの表示を若干スマートにした

--- command/info.js
+++ command/info.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const otenki = require('../otenki.js');
+const {infoEmbed} = require("../infoEmbed");
 
 const VERSION = '1.0.1';
 
@@ -8,12 +9,6 @@ module.exports = {
     name: 'info',
     description: 'info',
     async execute(client, command, args, message) {
-        message.channel.send(
-            new Discord.MessageEmbed()
-                .setTitle('おてんき bot')
-                .setColor('0x219ddd')
-                .setDescription('Version' + VERSION)
-                .setURL('https://github.com/Natsu-dev/otenki')
-        )
+        message.channel.send(infoEmbed(VERSION));
     }
 }
