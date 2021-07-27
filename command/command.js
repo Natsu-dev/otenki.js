4c9bf0c7896a32795d657b08df35280382ca0cfd
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-07-27 21:40:13 +0900

    [upd] Version 1.0.0

--- command/command.js
+++ command/command.js
@@ -1,15 +1,19 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 const client = new Discord.Client();
 module.exports = {
     name: 'command',
     description: 'command hub',
     async execute(client, command, args, message){
-        if (command === 'test'){
-            message.channel.send('test')
+        if (command === 'test' || command === 't'){
+            message.channel.send('test');
         }
-        if (command === 'forecast'){
-            await client.commands.forecast.execute(client, command, args, message)
+        if (command === 'forecast' || command === 'f'){
+            console.log('command: forecast');
+            await client.commands.get('forecast').execute(client, command, args, message);
+        }
+        if (command === ('info')){
+            console.log('command: info');
+            await client.commands.get('info').execute(client, command, args, message);
         }
     }
 }
--- otenki.js
+++ otenki.js
@@ -5,11 +5,10 @@ const prefix = 't:';
 const fs = require('fs');
 const path = require('path');
 const cron = require('node-cron');
-const https = require('https');
+const {getWholeWeather} = require('./getWeatherData.js');
 
 // 環境変数に.envを使う
 require('dotenv').config({path: path.join(__dirname, '.env')});
-
 
 // コマンド読み込み
 let command_count = 0
@@ -22,36 +21,10 @@ for (const file of commandFiles) {
 }
 console.log(`${command_count} files loaded.`)
 
-
-cron.schedule('0 * * * * *', () => {
-    let url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/010000.json';
-    https.get(url, (res) => {
-        let body = '';
-        res.setEncoding('utf8');
-
-        res.on('data', (chunk) => {
-            body += chunk;
-        });
-
-        res.on('end', (res) => {
-            res = JSON.parse(body);
-            console.log(res);
-            fs.writeFileSync('./tenki.json', JSON.stringify(res, null, '    '));
-        });
-    }).on('error', (e) => {
-        console.log(e.message); //エラー時
-    });
-},{
-    scheduled: true,
-    timezone: 'Asia/Tokyo'
-})
-
-
 // 定期実行の設定
-cron.schedule('0 * * * * *', () => {
+cron.schedule('0 0 22 * * *', () => {
     client.guilds.cache.forEach(guild => {
-        let channel;
-        channel = guild.channels.cache
+        let channel = guild.channels.cache
             .find(channel => channel.type === 'text'
                 && channel.permissionsFor(guild.me).has('SEND_MESSAGES')
                 && channel.name.indexOf('天気予報') > -1)
@@ -60,7 +33,7 @@ cron.schedule('0 * * * * *', () => {
                 .find(channel => channel.type === 'text'
                     && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
         }
-        channel.send(`${new Date()}`);
+        getWholeWeather().then(resolve => channel.send(resolve));
     })
 }, {
     scheduled: true,
@@ -103,14 +76,21 @@ client.on('message', async message => {
 
 });
 
-
+// サーバー参加時
 client.on("guildCreate", guild => {
     let greetingChannel;
     greetingChannel = guild.channels.cache.find(channel => channel.type === 'text' && channel.name.indexOf('天気予報') > -1)
     if (greetingChannel === undefined) {
         greetingChannel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
     }
-    greetingChannel.send('guildCreateGreeting');
+    greetingChannel.send(
+        // TODO ここ仮実装、きたないので後で直そう
+        new Discord.MessageEmbed()
+            .setTitle('おてんき bot')
+            .setColor('0x219ddd')
+            .setDescription('Version 1.0.0')
+            .setURL('https://github.com/Natsu-dev/otenki')
+    );
 });
 
 client.login(process.env.DISCORD_TOKEN) // Login phase
